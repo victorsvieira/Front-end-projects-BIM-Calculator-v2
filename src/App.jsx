79ff5800bec3d3459c4838cf69db0d8926ee97ea
Victor de Souza Vieira <victordesouzavieira@gmail.com>
2025-09-13 21:58:12 -0300

    Start

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Calculator from './components/Calculator'
+import ExtraInfo from './components/ExtraInfo'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className='Homepage'>
+        <h1>CALCULADORA DE IMC (Índice de Massa Comporal)</h1>
+        <Calculator />
+        <ExtraInfo />
+       
+      </div>
+    </>
+  )
+}
+
+export default App
