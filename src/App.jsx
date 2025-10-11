8b40954cf28b434b6e86111c8499b8595ef7611d
Victor de Souza Vieira <victordesouzavieira@gmail.com>
2025-10-11 20:42:20 -0300

    fixed deploy

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import React, { useState, useEffect } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { useState, useEffect } from "react";
 import "./App.css";
 import Calculator from "./components/Calculator";
 import ExtraInfo from "./components/ExtraInfo";
 
 function App() {
   const [count, setCount] = useState(0);
-  const [darkMode, setDarkMode] = useState(
-    () => localStorage.getItem("darkMode") === "true"
-  );
+  const [darkMode, setDarkMode] = useState(() => localStorage.getItem("darkMode") === "true");
 
   const handleDarkMode = () => {
     setDarkMode(!darkMode);
@@ -24,10 +20,7 @@ function App() {
       <div className={darkMode ? "main-content dark" : "main-content"}>
         <div className={darkMode ? "header dark" : "header"}>
           <h1>CALCULADORA DE IMC (Índice de Massa Corporal)</h1>
-          <button
-            onClick={handleDarkMode}
-            className={darkMode ? "btn-darkMode dark" : "btn-darkMode"}
-          >
+          <button onClick={handleDarkMode} className={darkMode ? "btn-darkMode dark" : "btn-darkMode"}>
             <i className={darkMode ? "bx  bx-moon dark" : "bx  bx-moon"}></i>
           </button>
         </div>
